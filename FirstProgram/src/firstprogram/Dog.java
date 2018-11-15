430dc2616239d5d40234fef0e355f214400a51c9
royWati <royywati@gmail.com>
2018-11-15 10:52:16 +0300

    Initial Commit

---
+++ FirstProgram/src/firstprogram/Dog.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstprogram;
+
+/**
+ *
+ * @author folio
+ */
+
+// this is our class which is called a Dog
+public class Dog {
+    
+    /* 
+        we need a main method to execute our Dog class
+        hence we create  a main method
+    */  
+            public static void main(String[] args){
+                /*
+                    when writing a line of code 
+                    we are supposed to break it using 
+                    ';' i.e semi-colon before moving to
+                    the next line of code 
+                    otherwise the compiler will recognise
+                    thw two lines of code as one line
+                 */
+                /* System is class found inside a method the jdk
+                    that allows us to input and output data on the
+                    program
+                */
+                System.out.println("hello world!!!");
+            }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
